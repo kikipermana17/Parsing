1bacd5d0fc68b04d65bbb9408c48c1ff752de297
kikipermana17 <bima_235@smkassalaambandung.sch.id>
2022-03-16 21:20:38 -0700

    parsing

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:parsing/_json.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:parsing/profile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -40,46 +41,56 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Read Json'),
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      _loadSampleJson();
-                    },
-                    child: Container(
-                      height: 50,
-                      width: 150,
-                      color: Colors.grey,
-                      alignment: Alignment.center,
-                      child: const Text("Read JSON File"),
-                    ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 12.0),
-                child: Text(
-                  _jsonContent,
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ],
+    return MaterialApp(
+      title: "Parsing",
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Flutter"),
+            backgroundColor: Colors.blueAccent,
           ),
-        ),
-      ),
+          body: Profile()),
     );
+
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: const Text('Read Json'),
+    //   ),
+    //   body: SafeArea(
+    //     child: Padding(
+    //       padding: const EdgeInsets.all(16.0),
+    //       child: Column(
+    //         mainAxisAlignment: MainAxisAlignment.center,
+    //         mainAxisSize: MainAxisSize.max,
+    //         crossAxisAlignment: CrossAxisAlignment.stretch,
+    //         children: <Widget>[
+    //           Row(
+    //             mainAxisAlignment: MainAxisAlignment.center,
+    //             children: [
+    //               GestureDetector(
+    //                 onTap: () {
+    //                   _loadSampleJson();
+    //                 },
+    //                 child: Container(
+    //                   height: 50,
+    //                   width: 150,
+    //                   color: Colors.grey,
+    //                   alignment: Alignment.center,
+    //                   child: const Text("Read JSON File"),
+    //                 ),
+    //               ),
+    //             ],
+    //           ),
+    //           Padding(
+    //             padding: const EdgeInsets.only(top: 12.0),
+    //             child: Text(
+    //               _jsonContent,
+    //               textAlign: TextAlign.center,
+    //             ),
+    //           ),
+    //         ],
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
